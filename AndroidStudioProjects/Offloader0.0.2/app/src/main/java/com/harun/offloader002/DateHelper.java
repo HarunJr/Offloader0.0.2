f11f58aedaada10779ffad6a333cc25fb3369d7e
HARUN <harunm28@gmmail.com>
2016-05-13 18:58:05 +0300

    DateHelper

--- AndroidStudioProjects/Offloader0.0.2/app/src/main/java/com/harun/offloader002/DateHelper.java
+++ AndroidStudioProjects/Offloader0.0.2/app/src/main/java/com/harun/offloader002/DateHelper.java
@@ -1,7 +1,34 @@
 package com.harun.offloader002;
 
+import com.harun.offloader002.adapters.DetailsCursorAdapter;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
 /**
  * Created by HARUN on 5/13/2016.
  */
 public class DateHelper {
+    public static final String LOG_TAG = DateHelper.class.getSimpleName();
+
+    public static String getFormattedDateTimeString(DetailsCursorAdapter detailsCursorAdapter, long dateInMillis){
+
+        DateTimeFormatter dateTimeFormat = DateTimeFormat.forPattern("dd/MMMM/yyyy h:mm a");
+        DateTime dateTime = new DateTime(Long.valueOf(dateInMillis), DateTimeZone.UTC);
+//        Log.w(LOG_TAG, "DateTime: " + dateTime);
+        return dateTimeFormat.print(dateTime);
+    }
+
+    public static String getFormattedDayString(long dateInMillis){
+        DateTimeFormatter dayFormat = DateTimeFormat.forPattern("dd/MMMM/yyyy");
+        DateTime dateTime = new DateTime(Long.valueOf(dateInMillis), DateTimeZone.UTC);
+        return dayFormat.print(dateTime);
+    }
+    public static String getFormattedTimeString(long dateInMillis){
+        DateTimeFormatter dayFormat = DateTimeFormat.forPattern(" h:mm a");
+        DateTime dateTime = new DateTime(Long.valueOf(dateInMillis), DateTimeZone.UTC);
+        return dayFormat.print(dateTime);
+    }
 }
