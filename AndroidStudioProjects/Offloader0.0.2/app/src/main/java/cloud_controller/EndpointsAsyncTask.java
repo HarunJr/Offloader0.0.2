fd93f5991e545aeccfad71cf73e7a1021e5c0274
HARUN <harunm28@gmmail.com>
2016-05-29 10:11:03 +0300

    collections and expenses sql

---
+++ AndroidStudioProjects/Offloader0.0.2/app/src/main/java/cloud_controller/EndpointsAsyncTask.java
@@ -0,0 +1,7 @@
+package cloud_controller;
+
+/**
+ * Created by HARUN on 5/25/2016.
+ */
+public class EndpointsAsyncTask {
+}
---
+++ AndroidStudioProjects/Offloader0.0.2/app/src/main/java/com/harun/offloader002/DialogInputFragment.java
@@ -0,0 +1,7 @@
+package com.harun.offloader002;
+
+/**
+ * Created by HARUN on 5/14/2016.
+ */
+public class DialogInputFragment {
+}
